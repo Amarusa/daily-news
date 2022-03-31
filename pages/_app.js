9a57be68f9f94054ba7ce6d7d56410e5d777459a
gus199 <augustine2903a@gmail.com>
2022-03-30 21:28:40 -0400

    Add Authentication for the app using firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {AuthContextProvider} from '../context/AuthContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthContextProvider>
+      <Navbar />
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
